efaea4b69273b88f49403487b503168300aa6fd1
fenezglonxiy <thdathcmus@gmail.com>
2024-12-08 15:16:06 +0700

    PW-5 Update HeaderContent and add 'flow-spacer' global classes

--- src/_pages/header/HeaderContent.tsx
+++ src/_pages/header/HeaderContent.tsx
@@ -9,10 +9,15 @@ import getHeaderContentCss from "./getHeaderContentCss";
 type HeaderContentProps = React.HTMLAttributes<HTMLDivElement>;
 
 function HeaderContent(props: HeaderContentProps) {
+  const { children, ...rest } = props;
   const theme = useTheme();
   const css = getHeaderContentCss(theme);
 
-  return <div css={css.root} {...props} />;
+  return (
+    <div css={css.root} {...rest}>
+      <div css={css.container}>{children}</div>
+    </div>
+  );
 }
 
 export default HeaderContent;
--- src/_pages/header/getHeaderContentCss.ts
+++ src/_pages/header/getHeaderContentCss.ts
@@ -1,16 +1,20 @@
 import { css, Theme } from "@mui/material";
 
 const getHeaderContentRootCss = (theme: Theme) => css`
-  max-width: 1080px;
-  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  padding: ${theme.spacing(4)} ${theme.spacing(15)};
+`;
+
+const getHeaderContentContainerCss = (theme: Theme) => css`
   display: flex;
   align-items: center;
   gap: ${theme.spacing(8)};
-  padding: ${theme.spacing(4)} ${theme.spacing(15)};
 `;
 
 const getHeaderContentCss = (theme: Theme) => ({
   root: getHeaderContentRootCss(theme),
+  container: getHeaderContentContainerCss(theme),
 });
 
 export default getHeaderContentCss;
