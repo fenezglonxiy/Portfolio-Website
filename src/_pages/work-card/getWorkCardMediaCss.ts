68c307128dd452001ad9a6f0e0a9711187923022
fenezglonxiy <thdathcmus@gmail.com>
2024-12-11 21:34:24 +0700

    PW-5 Add WorkCardMedia height

--- src/_pages/work-card/getWorkCardMediaCss.ts
+++ src/_pages/work-card/getWorkCardMediaCss.ts
@@ -2,6 +2,10 @@ import { css, Theme } from "@mui/material";
 
 const getWorkCardMediaCss = (theme: Theme) => css`
   padding: ${theme.spacing(5)} ${theme.spacing(5)} 0;
+
+  & > * {
+    height: 320px;
+  }
 `;
 
 export default getWorkCardMediaCss;
