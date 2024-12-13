01170ccf58761487063fb6963a352459432b692d
fenezglonxiy <thdathcmus@gmail.com>
2024-12-13 15:43:19 +0700

    PW-5 Add ToolsInfiniteScroller component

---
+++ src/_pages/tools-infinite-scroller/ToolsInfiniteScroller.tsx
@@ -0,0 +1,33 @@
+import {
+  InfiniteScroller,
+  InfiniteScrollerContent,
+  InfiniteScrollerProps,
+} from "@/_components/infinite-scroller";
+import ToolInfiniteScrollerItem from "./ToolInfiniteScrollerItem";
+
+export type ToolsInfiniteScrollerProps = InfiniteScrollerProps & {
+  children?: undefined | null;
+};
+
+function ToolsInfiniteScroller(props: ToolsInfiniteScrollerProps) {
+  return (
+    <InfiniteScroller {...props}>
+      <InfiniteScrollerContent>
+        <ToolInfiniteScrollerItem
+          toolImageSrc="figma.png"
+          toolImageAlt="figma"
+        />
+        <ToolInfiniteScrollerItem
+          toolImageSrc="greylock.png"
+          toolImageAlt="greylock"
+        />
+        <ToolInfiniteScrollerItem
+          toolImageSrc="codecov.png"
+          toolImageAlt="codecov"
+        />
+      </InfiniteScrollerContent>
+    </InfiniteScroller>
+  );
+}
+
+export default ToolsInfiniteScroller;
