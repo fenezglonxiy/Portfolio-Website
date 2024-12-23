7eaf66b4d58e48ea28653163fb48516657e96fc6
fenezglonxiy <thdathcmus@gmail.com>
2024-12-23 16:28:56 +0700

    PW-9 Add AboutMeSkills section

---
+++ src/_pages/about-me-skills/getAboutMeSkillsHeaderCss.ts
@@ -0,0 +1,9 @@
+import { css } from "@mui/material";
+
+const getAboutMeSkillsHeaderCss = () => css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export default getAboutMeSkillsHeaderCss;
