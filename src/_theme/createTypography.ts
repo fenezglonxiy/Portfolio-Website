abd2e5d2dcdf2c10cbf66882b9d17f96f65249ff
fenezglonxiy <thdathcmus@gmail.com>
2025-01-25 16:21:09 +0700

    Change some colors and add Badge component styling

--- src/_theme/createTypography.ts
+++ src/_theme/createTypography.ts
@@ -218,7 +218,7 @@ const typography = (theme: Theme): Typography => ({
   fontSize: 14,
   fontWeightThin: fontWeightByType.thin,
   fontWeightExtraLight: fontWeightByType["extra-light"],
-  fontWeightLight: fontWeightByType.light,
+  fontWeightLight: fontWeightByType.main,
   fontWeightRegular: fontWeightByType.regular,
   fontWeightMedium: fontWeightByType.medium,
   fontWeightSemibold: fontWeightByType["semi-bold"],
