19fc75e36d99b425fc1b97b326cba2f3e8f37123
fenezglonxiy <thdathcmus@gmail.com>
2025-01-26 16:19:59 +0700

    Bumped version number to 1.0.2

--- src/_components/hero/HeroMedia.tsx
+++ src/_components/hero/HeroMedia.tsx
@@ -1,43 +1,9 @@
-import {
-  OverridableComponent,
-  OverrideProps,
-} from "@mui/material/OverridableComponent";
-import React from "react";
+import { Media, MediaProps } from "@/_components/media";
 
-import { Box } from "@/_components/box";
+export type HeroMediaProps = MediaProps;
 
-type HeroMediaBaseProps = {
-  /**
-   * Control the media source.
-   */
-  src: string;
+const HeroMedia = Media;
 
-  /**
-   * Control the alternative text for the media.
-   */
-  alt?: string;
-
-  /**
-   * Control the element used for the root node.
-   *
-   * It should match with the media type for the component work properly.
-   */
-  component: "video" | "audio" | "picture" | "iframe" | "img";
-};
-
-export interface HeroMediaTypeMap<D extends React.ElementType = "div"> {
-  props: HeroMediaBaseProps;
-  defaultComponent: D;
-}
-
-export type HeroMediaProps<
-  D extends React.ElementType = HeroMediaTypeMap["defaultComponent"]
-> = OverrideProps<HeroMediaTypeMap<D>, D> & { component?: React.ElementType };
-
-const HeroMedia: OverridableComponent<HeroMediaTypeMap> = React.forwardRef(
-  function HeroMedia(props: HeroMediaProps, ref: React.Ref<HTMLDivElement>) {
-    return <Box ref={ref} {...props} />;
-  }
-);
+HeroMedia.displayName = "HeroMedia";
 
 export default HeroMedia;
