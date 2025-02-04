9a44a13b01e1190ed09122c4e85798f5688a0675
fenezglonxiy <thdathcmus@gmail.com>
2025-02-04 11:18:10 +0700

    PW-21 Add navigating to 404 page when a specific work is not found

--- src/_pages/work-details-content/getSideDetailsSectionCss.ts
+++
@@ -1,7 +0,0 @@
-import { css, Theme } from "@mui/material";
-
-const getSideDetailsSectionCss = (theme: Theme) => css`
-  --flow-spacing-y: ${theme.spacing(6)};
-`;
-
-export default getSideDetailsSectionCss;
--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { addMonths } from "date-fns";
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 import { WorkDetailsContent } from "@/_pages/work-details-content";
 import { WorkDetailsHero } from "@/_pages/work-details-hero";
@@ -17,24 +18,35 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-const work = {
-  title: "Struktura.",
-  summary:
-    "Utilizing captivating imagery to enhance the overall user experience",
-  mediaSrc: "/image.png",
-  startDate: new Date(),
-  endDate: addMonths(new Date(), 2),
-  businessSectors: ["Construction"],
-  services: ["Web Design", "Web Development", "Mobile Development"],
-  about:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis aliquet nisl at eleifend. Donec ut ex interdum, malesuada turpis quis, rhoncus justo. Donec auctor lectus eu leo vestibulum accumsan. Nullam pretium rhoncus libero vel congue. Integer nec orci consequat, dignissim lectus id, rhoncus lorem. Aenean at malesuada sem. Ut a rutrum sem. Ut sit amet varius ligula. Suspendisse commodo, tortor vulputate hendrerit maximus, elit purus convallis justo, eget semper eros arcu in arcu. Donec ut venenatis lectus. Nullam non mi viverra elit placerat pretium venenatis quis sapien. Vivamus orci orci, scelerisque ut enim sed, sodales viverra magna. Sed cursus gravida malesuada. Nunc et varius neque.",
-  challenge:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis aliquet nisl at eleifend. Donec ut ex interdum, malesuada turpis quis, rhoncus justo. Donec auctor lectus eu leo vestibulum accumsan. Nullam pretium rhoncus libero vel congue. Integer nec orci consequat, dignissim lectus id, rhoncus lorem. Aenean at malesuada sem. Ut a rutrum sem. Ut sit amet varius ligula. Suspendisse commodo, tortor vulputate hendrerit maximus, elit purus convallis justo, eget semper eros arcu in arcu. Donec ut venenatis lectus. Nullam non mi viverra elit placerat pretium venenatis quis sapien. Vivamus orci orci, scelerisque ut enim sed, sodales viverra magna. Sed cursus gravida malesuada. Nunc et varius neque.",
-  results:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis aliquet nisl at eleifend. Donec ut ex interdum, malesuada turpis quis, rhoncus justo. Donec auctor lectus eu leo vestibulum accumsan. Nullam pretium rhoncus libero vel congue. Integer nec orci consequat, dignissim lectus id, rhoncus lorem. Aenean at malesuada sem. Ut a rutrum sem. Ut sit amet varius ligula. Suspendisse commodo, tortor vulputate hendrerit maximus, elit purus convallis justo, eget semper eros arcu in arcu. Donec ut venenatis lectus. Nullam non mi viverra elit placerat pretium venenatis quis sapien. Vivamus orci orci, scelerisque ut enim sed, sodales viverra magna. Sed cursus gravida malesuada. Nunc et varius neque.",
+const titleToWork = {
+  struktura: {
+    title: "Struktura.",
+    summary:
+      "Utilizing captivating imagery to enhance the overall user experience",
+    mediaSrc: "/image.png",
+    startDate: new Date(),
+    endDate: addMonths(new Date(), 2),
+    businessSectors: ["Construction"],
+    services: ["Web Design", "Web Development", "Mobile Development"],
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis aliquet nisl at eleifend. Donec ut ex interdum, malesuada turpis quis, rhoncus justo. Donec auctor lectus eu leo vestibulum accumsan. Nullam pretium rhoncus libero vel congue. Integer nec orci consequat, dignissim lectus id, rhoncus lorem. Aenean at malesuada sem. Ut a rutrum sem. Ut sit amet varius ligula. Suspendisse commodo, tortor vulputate hendrerit maximus, elit purus convallis justo, eget semper eros arcu in arcu. Donec ut venenatis lectus. Nullam non mi viverra elit placerat pretium venenatis quis sapien. Vivamus orci orci, scelerisque ut enim sed, sodales viverra magna. Sed cursus gravida malesuada. Nunc et varius neque.",
+    challenge:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis aliquet nisl at eleifend. Donec ut ex interdum, malesuada turpis quis, rhoncus justo. Donec auctor lectus eu leo vestibulum accumsan. Nullam pretium rhoncus libero vel congue. Integer nec orci consequat, dignissim lectus id, rhoncus lorem. Aenean at malesuada sem. Ut a rutrum sem. Ut sit amet varius ligula. Suspendisse commodo, tortor vulputate hendrerit maximus, elit purus convallis justo, eget semper eros arcu in arcu. Donec ut venenatis lectus. Nullam non mi viverra elit placerat pretium venenatis quis sapien. Vivamus orci orci, scelerisque ut enim sed, sodales viverra magna. Sed cursus gravida malesuada. Nunc et varius neque.",
+    results:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis aliquet nisl at eleifend. Donec ut ex interdum, malesuada turpis quis, rhoncus justo. Donec auctor lectus eu leo vestibulum accumsan. Nullam pretium rhoncus libero vel congue. Integer nec orci consequat, dignissim lectus id, rhoncus lorem. Aenean at malesuada sem. Ut a rutrum sem. Ut sit amet varius ligula. Suspendisse commodo, tortor vulputate hendrerit maximus, elit purus convallis justo, eget semper eros arcu in arcu. Donec ut venenatis lectus. Nullam non mi viverra elit placerat pretium venenatis quis sapien. Vivamus orci orci, scelerisque ut enim sed, sodales viverra magna. Sed cursus gravida malesuada. Nunc et varius neque.",
+  },
 };
 
-export default function WorkDetails(_: Props) {
+export default async function WorkDetails(props: Props) {
+  const { params } = props;
+  const slug = (await params).slug;
+
+  if (!Object.keys(titleToWork).includes(slug)) {
+    notFound();
+  }
+
+  const work = titleToWork[slug as keyof typeof titleToWork];
+
   return (
     <article>
       <WorkDetailsHero
