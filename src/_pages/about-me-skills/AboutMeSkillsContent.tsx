63eb9b34f6ce8792c064073b8e998b49795df813
fenezglonxiy <thdathcmus@gmail.com>
2025-03-06 14:20:30 +0700

    PW-9 Fix errors

--- src/_pages/about-me-skills/AboutMeSkillsContent.tsx
+++ src/_pages/about-me-skills/AboutMeSkillsContent.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { styled } from "@mui/material";
 import memoTheme from "@mui/material/utils/memoTheme";
+import clsx from "clsx";
+
+import aboutMeSkillsContentClasses from "./aboutMeSkillsContentClasses";
 
 type AboutMeSkillsContentRootProps = React.ComponentPropsWithoutRef<"div">;
 
@@ -21,7 +24,13 @@ const AboutMeSkillsContent = React.forwardRef(function AboutMeSkillsContent(
 ) {
   const { className, ...rest } = props;
 
-  return <AboutMeSkillsContentRoot ref={ref} {...rest} />;
+  return (
+    <AboutMeSkillsContentRoot
+      ref={ref}
+      className={clsx(aboutMeSkillsContentClasses.root, className)}
+      {...rest}
+    />
+  );
 });
 
 AboutMeSkillsContent.displayName = "AboutMeSkillsContent";
